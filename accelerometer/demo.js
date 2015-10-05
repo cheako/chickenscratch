845f8eda7cdcba707be570f307667d1b97a85f05
Michael Mestnik <cheako+github_com@mikemestnik.net>
2015-10-05 14:20:32 -0500

    Try with an enter button and full screen.

--- accelerometer/demo.js
+++ accelerometer/demo.js
@@ -1,53 +1,86 @@
-screen.orientation.lock("portrait-primary");
+function boundingBoxCheck() {
+    if (x < 0) {
+        x = 0;
+        vx = -vx;
+    }
+    if (y < 0) {
+        y = 0;
+        vy = -vy;
+    }
+    if (x > document.documentElement.clientWidth - 20) {
+        x = document.documentElement.clientWidth - 20;
+        vx = -vx;
+    }
+    if (y > document.documentElement.clientHeight - 20) {
+        y = document.documentElement.clientHeight - 20;
+        vy = -vy;
+    }
+}
 
-var x = 0, y = 0,
-    vx = 0, vy = 0,
-	ax = 0, ay = 0;
-	
-var sphere = document.getElementById("sphere");
+function main_enter() {
 
-if (window.DeviceMotionEvent != undefined) {
-	window.ondevicemotion = function(e) {
-		ax = event.accelerationIncludingGravity.x * 5;
-		ay = event.accelerationIncludingGravity.y * 5;
-		document.getElementById("accelerationX").innerHTML = e.accelerationIncludingGravity.x;
-		document.getElementById("accelerationY").innerHTML = e.accelerationIncludingGravity.y;
-		document.getElementById("accelerationZ").innerHTML = e.accelerationIncludingGravity.z;
+    document.getElementById("splash").style.display = "none";
+    document.getElementById("content").style.display = "block";
+    if (!document.fullscreenElement && // alternative standard method
+        !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) { // current working methods
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        throw "Can't fullscreen."
+    }
 
-		if ( e.rotationRate ) {
-			document.getElementById("rotationAlpha").innerHTML = e.rotationRate.alpha;
-			document.getElementById("rotationBeta").innerHTML = e.rotationRate.beta;
-			document.getElementById("rotationGamma").innerHTML = e.rotationRate.gamma;
-		}		
-	}
+    screen.orientation.lock("portrait-primary");
 
-	setInterval( function() {
-		var landscapeOrientation = window.innerWidth/window.innerHeight > 1;
-		if ( landscapeOrientation) {
-			vx = vx + ay;
-			vy = vy + ax;
-		} else {
-			vy = vy - ay;
-			vx = vx + ax;
-		}
-		vx = vx * 0.98;
-		vy = vy * 0.98;
-		y = parseInt(y + vy / 50);
-		x = parseInt(x + vx / 50);
-		
-		boundingBoxCheck();
-		
-		sphere.style.top = y + "px";
-		sphere.style.left = x + "px";
-		
-	}, 25);
-} 
+    var x = 0,
+        y = 0,
+        vx = 0,
+        vy = 0,
+        ax = 0,
+        ay = 0;
 
+    var sphere = document.getElementById("sphere");
 
-function boundingBoxCheck(){
-	if (x<0) { x = 0; vx = -vx; }
-	if (y<0) { y = 0; vy = -vy; }
-	if (x>document.documentElement.clientWidth-20) { x = document.documentElement.clientWidth-20; vx = -vx; }
-	if (y>document.documentElement.clientHeight-20) { y = document.documentElement.clientHeight-20; vy = -vy; }
-	
+    if (window.DeviceMotionEvent != undefined) {
+        window.ondevicemotion = function(e) {
+            ax = event.accelerationIncludingGravity.x * 5;
+            ay = event.accelerationIncludingGravity.y * 5;
+            document.getElementById("accelerationX").innerHTML = e.accelerationIncludingGravity.x;
+            document.getElementById("accelerationY").innerHTML = e.accelerationIncludingGravity.y;
+            document.getElementById("accelerationZ").innerHTML = e.accelerationIncludingGravity.z;
+
+            if (e.rotationRate) {
+                document.getElementById("rotationAlpha").innerHTML = e.rotationRate.alpha;
+                document.getElementById("rotationBeta").innerHTML = e.rotationRate.beta;
+                document.getElementById("rotationGamma").innerHTML = e.rotationRate.gamma;
+            }
+        }
+
+        setInterval(function() {
+            var landscapeOrientation = window.innerWidth / window.innerHeight > 1;
+            if (landscapeOrientation) {
+                vx = vx + ay;
+                vy = vy + ax;
+            } else {
+                vy = vy - ay;
+                vx = vx + ax;
+            }
+            vx = vx * 0.98;
+            vy = vy * 0.98;
+            y = parseInt(y + vy / 50);
+            x = parseInt(x + vx / 50);
+
+            boundingBoxCheck();
+
+            sphere.style.top = y + "px";
+            sphere.style.left = x + "px";
+
+        }, 25);
+    }
 }
