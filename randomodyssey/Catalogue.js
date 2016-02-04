b0aa83f0c23f544baf9fdba19413d6a114983b1b
Michael Mestnik <cheako+github_com@mikemestnik.net>
2016-02-04 09:42:19 -0600

    Update Catalogue.js

--- randomodyssey/Catalogue.js
+++ randomodyssey/Catalogue.js
@@ -1,2 +1,43 @@
-Items = [{type:'Axe',cost:50,level:1,odds:1,onAttack:function(m){m.hp-=5;}}];
-Affixes = [[{type:"Tiger's",cost:100,level:5,odds:0.3,onAttack:function(m){m.hp-=15;}}],[{type:'of fire',cost:75,level:1,odds:0.7,onAttack:function(m){m.hp-=3;}}]];
+Items = [{
+    type: 'Axe',
+    cost: 50,
+    level: 1,
+    odds: 1,
+    onAttack: function(m) {
+        m.hp -= 5;
+    }
+}];
+Affixes = [
+    [{
+        type: "Tiger's",
+        cost: 100,
+        level: 5,
+        odds: 0.3,
+        onAttack: function(m) {
+            m.hp -= 15;
+        }
+    }],
+    [{
+        type: 'of fire',
+        cost: 75,
+        level: 1,
+        odds: 0.7,
+        onAttack: function(m) {
+            m.hp -= 3;
+        }
+    }]
+];
+TerrainTiles = [{
+    style: "yellow",
+    type: "$"
+  }, {
+    style: "lime",
+    type: "F"
+  },
+  {
+    style: "lime",
+    type: "V"
+  }, {
+    style: "olive",
+    type: "M"
+  }];
